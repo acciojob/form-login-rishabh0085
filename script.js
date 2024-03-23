9dee458e525eb2643ae27d207653c7a4b572d33d
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2024-03-23 18:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Access the form elements
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
 
+    // Concatenate the first name and last name
+    const fullName = firstName + ' ' + lastName;
+
+    // Display the full name using an alert
+    alert(fullName);
 }
